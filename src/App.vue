83108e66ae3b21e2424734eb58fab8a4fbb253ba
qazx7412 <git@newgenerations.im>
2018-09-12 18:26:41 +0900

    ナビバー

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,23 @@
 <template>
-  <div id="header">
-    <h1>{{ title }}</h1>
-    <h2>{{ name }}</h2>
-    <p>勢いでドメイン取っちゃったんだけどなにしよ…</p>
-    <p>なんもアイディア思いつかない</p>
-    <!-- <router-view/> -->
+  <div id="app">
+    <appheader/>
+    <appnav/>
+    <router-view/>
   </div>
 </template>
 
 <script>
+import appheader from './components/appheader'
+import appnav from './components/appnav'
+
 export default {
-  name: 'header',
-  data () {
-    return {
-      title: 'new generations',
-      name: 'qazx7412,kairox,回路'
-    }
-  }
+  components: {
+    appheader,
+    appnav
+  },
+  name: 'app'
 }
 </script>
 
 <style>
-body{
-  margin: 0;
-}
-
-#header{
-  text-align: center;
-  line-height: 1;
-  padding: 4.2vw 16.8vw 0.84vw;  /*あとで変える 50px 200px 10px*/
-  background: url('/static/hed.png') center center;
-  background-blend-mode: multiply;
-  background-size: cover;
-  background-color: #b5b5b5 ;
-}
-
-#header h1{
-  font-size: 3.5vw;
-  color: #FFFFFF;
-  line-height: 0;
-  font-weight: normal;
-}
-
-#header h2{
-  font-size: 2vw;
-  color: #FFFFFF;
-  line-height: 2;
-  font-weight: normal;
-}
-
-#header p{
-  font-size: 1vw;
-  color: #FFFFFF;
-  line-height: 0;
-  font-weight: normal;
-}
-
-@media screen and (max-width:990px) {
-  #header p{
-    display: none;
-  }
-}
 </style>
---
+++ src/components/appnav.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="appnav">
+    <nav>
+      <ul class="gnav">
+        <li v-for="(link, key, index) in links" :key="index">
+          <a :href=link.url>{{ link.name }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      links: [
+        {
+          name: 'twitter',
+          url: 'https://twitter.com/qazx7412'
+        },
+        {
+          name: 'imastodon',
+          url: 'https://imastodon.net/@qazx7412'
+        },
+        {
+          name: 'mstdn.jp',
+          url: 'https://mstdn.jp/@qazx7412'
+        },
+        {
+          name: 'github',
+          url: 'https://github.com/limit7412'
+        },
+        {
+          name: 'gitlab',
+          url: 'https://gitlab.com/qazx7412'
+        },
+        {
+          name: '走るゲーム',
+          url: 'running_event/index.html'
+        },
+        {
+          name: 'exit',
+          url: 'https://imastodon.net/'
+        }
+      ]
+    }
+  }
+
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+}
+
+.gnav{
+  margin-top: 3px;
+  margin-bottom: 3px;
+  text-align: center;
+  display: flex !important;
+  list-style-type: none;
+  font-weight:bold;
+  font-weight: normal;
+}
+
+.gnav li a{
+  display: block;
+  padding: 10px 8px;
+  color: #000;
+  text-decoration: none;
+  font-size: 1.4vw;
+  font-weight: normal;
+}
+
+</style>
