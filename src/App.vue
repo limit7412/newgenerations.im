20a3452dd96f28ed9879825d4bb870d72f3465bd
qazx7412 <git@newgenerations.im>
2018-09-12 17:31:05 +0900

    ついでにきれいに

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <h2>{{ name }}</h2>
     <p>勢いでドメイン取っちゃったんだけどなにしよ…</p>
     <p>なんもアイディア思いつかない</p>
+    <!-- <router-view/> -->
   </div>
 </template>
 
@@ -38,18 +39,21 @@ body{
   font-size: 3.5vw;
   color: #FFFFFF;
   line-height: 0;
+  font-weight: normal;
 }
 
 #header h2{
   font-size: 2vw;
   color: #FFFFFF;
   line-height: 2;
+  font-weight: normal;
 }
 
 #header p{
   font-size: 1vw;
   color: #FFFFFF;
   line-height: 0;
+  font-weight: normal;
 }
 
 @media screen and (max-width:990px) {
